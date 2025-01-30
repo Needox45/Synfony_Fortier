104e15bbf923f2e48e1b2f020d5ad53e71dbff58
Needox_ <romanfortier.contact@gmail.com>
2025-01-30 23:13:46 +0100

    1er test du list et show
    pas beau (css)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
-
 
 final class ArticleController extends AbstractController
 {
@@ -19,23 +18,39 @@ final class ArticleController extends AbstractController
         ]);
     }
 
-
     #[Route('/article/generate', name: 'generate_article')]
-        public function generateArticlet(EntityManagerInterface $entityManager): Response
-        {
+    public function generateArticle(EntityManagerInterface $entityManager): Response
+    {
         $article = new Article();
         $str_now = date('Y-m-d H:i:s', time());
-        $article->setTitre('Titre aleatoire #'.$str_now);
+        $article->setTitre('Roman' . $str_now);
         $content = file_get_contents('http://loripsum.net/api');
         $article->setTexte($content);
         $article->setPublie(true);
-        $article->setDate(\DateTimeImmutable::createFromFormat('Y-m-d H:i:s',
-        $str_now));
-            // tell Doctrine you want to (eventually) save the Product (no queries yet)
-            $entityManager->persist($article);
-            // actually executes the queries (i.e. the INSERT query)
-            $entityManager->flush();
-            return new Response('Saved new article with id '.$article->getId());
-        }
+        $article->setDate(\DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $str_now));
+        $entityManager->persist($article);
+        $entityManager->flush();
+        return new Response('Saved new article with id ' . $article->getId());
+    }
 
+    #[Route('/article/list', name: 'list_article')]
+    public function list(EntityManagerInterface $entityManager): Response
+    {
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+        return $this->render('article/list.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/article/show/{id}', name: 'show_article')]
+    public function show(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $article = $entityManager->getRepository(Article::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('The article does not exist');
+        }
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
 }
